1497a34eec6add9a02d63f0840bac310d5b36240
Veronika Aas <veronikaaas@live.no>
2024-11-18 21:54:40 +0100

    style: added custom colors from styleguide to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{html,js,jsx,ts,tsx}",  // Endre stier etter mappestrukturen
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#FAFAF8',
+        'primary': '#0C2C40',
+        'secondary': '#733D4B',
+        'accent': '#4F5D3C',
+        'copy': '#0C0C0C',
+      },
+    },
   },
   plugins: [],
 }
