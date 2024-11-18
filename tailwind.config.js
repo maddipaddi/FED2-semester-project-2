ddc8f5ede8c881dd5b0696db42289475b86cb32e
Veronika Aas <veronikaaas@live.no>
2024-11-18 21:25:55 +0100

    build: configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Endre stier etter mappestrukturen
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
