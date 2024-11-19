499e65eff5f86fe80426c050dc26e6be0db649af
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-19 18:45:57 +0100

    fix: change Tailwind config to ES modules instead of CommonJS to avoid errors and Vite crashing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-module.exports = {
-  darkMode: 'class',
+export default {
+  darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{html,js,jsx,ts,tsx}",  // Endre stier etter mappestrukturen
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Endre stier etter mappestrukturen
   ],
   theme: {
     extend: {
       colors: {
-        'background': '#FAFAF8',
-        'primary': '#0C2C40',
-        'secondary': '#733D4B',
-        'accent': '#4F5D3C',
-        'copy': '#0C0C0C',
+        background: "#FAFAF8",
+        primary: "#0C2C40",
+        secondary: "#733D4B",
+        accent: "#4F5D3C",
+        copy: "#0C0C0C",
       },
       fontFamily: {
-        serif: ['Baskervville', 'serif'],
-        sans: ['Roboto, sans-serif'],
+        serif: ["Baskervville", "serif"],
+        sans: ["Roboto, sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
