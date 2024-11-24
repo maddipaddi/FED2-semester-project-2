290d2a36f76bf6fc127673db24519c955eb321ba
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-24 16:11:04 +0100

    feat: implement initial templates for displaying single listings and listings by category

---
+++ src/js/ui/templates/displayListings.mjs
@@ -0,0 +1,37 @@
+// starting point: not finished with all info
+
+export function displayListings(listings) {
+  
+    return listings.map((listing) => {
+    const listingContainer = document.createElement("div");
+
+      const mediaContainer = document.createElement("div");
+
+      if (listing.media && listing.media.length > 0) {
+  const firstMedia = listing.media[0]; 
+  const media = document.createElement("img");
+  media.setAttribute("src", firstMedia.url); 
+  mediaContainer.appendChild(media); 
+}
+
+    const currentBid = document.createElement("p");
+    currentBid.innerText = `Current bid: `;
+
+    const endingDate = document.createElement("p");
+    endingDate.innerText = `Ending: `;
+
+    const bidButton = document.createElement("a");
+    bidButton.setAttribute("href", `/listing/read/?id=${listing.id}`);
+    bidButton.innerText = "Bid";
+
+    listingContainer.append(
+      mediaContainer,
+      currentBid,
+      endingDate,
+      bidButton
+    );
+
+  return listingContainer;
+  
+  });
+}
