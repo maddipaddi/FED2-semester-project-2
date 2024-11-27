7b2d25bcc095e3a996b210edc02bac78b6cce5b6
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-27 09:54:30 +0100

    feat: add function read listings by profile

--- src/js/ui/listing/read.mjs
+++ src/js/ui/listing/read.mjs
@@ -1,14 +1,26 @@
-import { fetchListings } from "../../api/listing/read.mjs";
+import {
+  fetchListings,
+  fetchListingsByProfile,
+} from "../../api/listing/read.mjs";
 
-export async function readListingsByCategory () {
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
-const options = {
-  _tag: category,
-}
+export async function readListingsByCategory() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const category = urlParams.get("category");
+  const options = {
+    _tag: category,
+  };
   try {
     // Fetch listings based on the category
     const listings = await fetchListings(options);
+    return listings;
+  } catch (error) {
+    console.error("Error loading listings:", error); // implement error handling
+  }
+}
+
+export async function readListingsByProfile(name) {
+  try {
+    const listings = await fetchListingsByProfile(name);
     return listings;
   } catch (error) {
     console.error("Error loading listings:", error); // implement error handling
