d226ecd03d62d35a5838d4214c1349e92939dc81
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-27 09:53:14 +0100

    feat: add fetching functionality for single listing and by profile

--- src/js/api/listing/read.mjs
+++ src/js/api/listing/read.mjs
@@ -1,5 +1,24 @@
-import { API_AUCTION_LISTINGS } from "../constants.mjs";
+import { API_AUCTION_LISTINGS, API_AUCTION_PROFILES } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
+
+export const fetchSingleListing = async (id, options = {}) => {
+  try {
+    if (!id) {
+      throw new Error("Read requires a post ID");
+    }
+
+    const query = new URLSearchParams(options);
+    const url = `${API_AUCTION_LISTINGS}/${id}?${query.toString()}`;
+
+    const response = await authFetch(url);
+    if (!response.ok) throw new Error("Failed to fetch listings");
+    const data = await response.json();
+    const listing = data.data;
+    return listing;
+  } catch (error) {
+    console.error("Error fetching listing:", error); // for dev purposes, remember to add user-friendly messages displayed to user later
+  }
+};
 
 export const fetchListings = async (options = {}) => {
   const query = new URLSearchParams(options);
@@ -8,7 +27,21 @@ export const fetchListings = async (options = {}) => {
     const response = await authFetch(url);
     if (!response.ok) throw new Error("Failed to fetch listings");
     const data = await response.json();
-    const listings = data.data; 
+    const listings = data.data;
+    return listings;
+  } catch (error) {
+    console.error("Error fetching listings:", error); // for dev purposes, remember to add user-friendly messages displayed to user later
+  }
+};
+
+export const fetchListingsByProfile = async (name, options = {}) => {
+  const query = new URLSearchParams(options);
+  const url = `${API_AUCTION_PROFILES}/${name}/listings?${query.toString()}`;
+  try {
+    const response = await authFetch(url);
+    if (!response.ok) throw new Error("Failed to fetch listings");
+    const data = await response.json();
+    const listings = data.data;
     return listings;
   } catch (error) {
     console.error("Error fetching listings:", error); // for dev purposes, remember to add user-friendly messages displayed to user later
