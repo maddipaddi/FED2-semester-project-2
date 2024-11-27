15cf1eac1c24e0e037661b69b217e56482ba04be
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-27 09:53:58 +0100

    fix: adjust link path to work

--- src/js/ui/templates/displayListings.mjs
+++ src/js/ui/templates/displayListings.mjs
@@ -1,18 +1,17 @@
 // starting point: not finished with all info
 
 export function displayListings(listings) {
-  
-    return listings.map((listing) => {
+  return listings.map((listing) => {
     const listingContainer = document.createElement("div");
 
-      const mediaContainer = document.createElement("div");
+    const mediaContainer = document.createElement("div");
 
-      if (listing.media && listing.media.length > 0) {
-  const firstMedia = listing.media[0]; 
-  const media = document.createElement("img");
-  media.setAttribute("src", firstMedia.url); 
-  mediaContainer.appendChild(media); 
-}
+    if (listing.media && listing.media.length > 0) {
+      const firstMedia = listing.media[0];
+      const media = document.createElement("img");
+      media.setAttribute("src", firstMedia.url);
+      mediaContainer.appendChild(media);
+    }
 
     const currentBid = document.createElement("p");
     currentBid.innerText = `Current bid: `;
@@ -21,17 +20,11 @@ export function displayListings(listings) {
     endingDate.innerText = `Ending: `;
 
     const bidButton = document.createElement("a");
-    bidButton.setAttribute("href", `/listing/read/?id=${listing.id}`);
+    bidButton.setAttribute("href", `/listing/read?id=${listing.id}`);
     bidButton.innerText = "Bid";
 
-    listingContainer.append(
-      mediaContainer,
-      currentBid,
-      endingDate,
-      bidButton
-    );
+    listingContainer.append(mediaContainer, currentBid, endingDate, bidButton);
 
-  return listingContainer;
-  
+    return listingContainer;
   });
 }
