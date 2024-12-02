d3586d42ebca4a721c69aed11621e46c39fa3436
Veronika Aas <veronikaaas@live.no>
2024-12-02 19:49:25 +0100

    style: change size on images

--- src/js/ui/templates/displaySingleListing.mjs
+++ src/js/ui/templates/displaySingleListing.mjs
@@ -19,17 +19,17 @@ export async function displaySingleListing(listing) {
   if (listing.media && listing.media.length > 0) {
     const mediaContainerMain = document.getElementById("media-container-main");
     const mediaContainer = document.getElementById("media-container");
-
+  
     listing.media.forEach((mediaItem, index) => {
       if (index === 0) {
         const mediaMain = document.createElement("img");
         mediaMain.setAttribute("src", mediaItem.url);
-        mediaMain.className = "h-64 w-full";
+        mediaMain.className = "h-96 w-full object-cover object-center"; 
         mediaContainerMain.appendChild(mediaMain);
       } else {
         const media = document.createElement("img");
         media.setAttribute("src", mediaItem.url);
-        media.className = "h-64 w-full h-32";
+        media.className = "h-56 w-full object-cover object-center"; 
         mediaContainer.append(media);
       }
     });
