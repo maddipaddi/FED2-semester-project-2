affd8a69eb2c09912511a419da296e3ac486be94
Veronika Aas <veronikaaas@live.no>
2024-12-02 20:57:21 +0100

    style: add styling to make a cohesive look throughout over site

--- src/js/ui/components/wishlist/displayWishlist.mjs
+++ src/js/ui/components/wishlist/displayWishlist.mjs
@@ -17,20 +17,61 @@ export async function displayWishlist() {
 
   listings.forEach((listing) => {
     const listingElement = document.createElement("div");
+    listingElement.classList.add(
+      "bg-white",
+      "shadow",
+      "rounded-lg",
+      "p-4",
+      "flex",
+      "flex-col",
+      "justify-between",
+      "mb-4"
+    );
 
     const mediaContainer = document.createElement("div");
+    mediaContainer.classList.add(
+      "w-full",
+      "h-52",
+      "bg-gray-200",
+      "rounded-md",
+      "overflow-hidden",
+      "mb-4"
+    );
+
     if (listing.media && listing.media.length > 0) {
       const firstMedia = listing.media[0];
       const media = document.createElement("img");
       media.setAttribute("src", firstMedia.url);
+      media.classList.add("w-full", "h-full", "object-cover");
       mediaContainer.appendChild(media);
     }
 
     const auctionEnd = document.createElement("p");
+    auctionEnd.classList.add(
+      "bg-primary", 
+      "text-background", 
+      "font-medium", 
+      "py-2", 
+      "px-4", 
+      "rounded-t-md", 
+      "text-center", 
+      "mb-2" 
+    );
     auctionEnd.innerText = `Ending: ${formatDateWithDayTimeDate(listing.endsAt)}`;
 
     const linkToReadListing = document.createElement("a");
     linkToReadListing.setAttribute("href", `/listing/read?id=${listing.id}`);
+    linkToReadListing.classList.add(
+      "bg-accent", 
+      "text-background",
+      "font-medium", 
+      "py-2", 
+      "px-4", 
+      "rounded-b-md", 
+      "block", 
+      "text-center", 
+      "hover:font-bold" 
+    );
     linkToReadListing.innerText = "Want to bid?";
 
     listingElement.append(mediaContainer, auctionEnd, linkToReadListing);
