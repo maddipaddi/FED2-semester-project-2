dcdb29931acdc92e5c7f25012ed8bffd0c5be9ad
Veronika Aas <veronikaaas@live.no>
2024-12-03 21:41:50 +0100

    style: add styling to my active bids to match the other pages

--- src/js/ui/templates/displayMyActiveListings.mjs
+++ src/js/ui/templates/displayMyActiveListings.mjs
@@ -3,16 +3,35 @@ import { onDeletePost } from "../listing/delete.mjs";
 export function displayMyActiveListings(listings) {
   return listings.map((listing) => {
     const listingContainer = document.createElement("div");
+    listingContainer.classList.add(
+      "bg-white",
+      "shadow",
+      "rounded-lg",
+      "p-4",
+      "flex",
+      "flex-col",
+      "justify-between",
+      "mb-4"
+    );
 
     const linkToReadListing = document.createElement("a");
     linkToReadListing.setAttribute("href", `/listing/read?id=${listing.id}`);
 
     const mediaContainer = document.createElement("div");
+    mediaContainer.classList.add(
+      "w-full",
+      "h-52",
+      "bg-gray-200",
+      "rounded-md",
+      "overflow-hidden",
+      "mb-4"
+    );
 
     if (listing.media && listing.media.length > 0) {
       const firstMedia = listing.media[0];
       const media = document.createElement("img");
       media.setAttribute("src", firstMedia.url);
+      media.classList.add("w-full", "h-full", "object-cover");
       mediaContainer.appendChild(media);
     }
 
@@ -20,10 +39,32 @@ export function displayMyActiveListings(listings) {
 
     const editButton = document.createElement("a");
     editButton.innerText = "Edit";
+    editButton.classList.add(
+      "bg-accent", 
+      "text-background", 
+      "font-medium", 
+      "py-2", 
+      "px-4", 
+      "rounded-t-md", 
+      "text-center",
+      "hover:font-bold",
+      "mb-2" 
+    );
     editButton.setAttribute("href", `/listing/update?id=${listing.id}`);
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
+    deleteButton.classList.add(
+      "bg-secondary", 
+      "text-background",
+      "font-medium", 
+      "py-2", 
+      "px-4", 
+      "rounded-b-md", 
+      "block", 
+      "text-center", 
+      "hover:font-bold" 
+    );
     deleteButton.setAttribute("id", `${listing.id}`);
     deleteButton.addEventListener("click", onDeletePost);
 
